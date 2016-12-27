85ae72993aa7c34ce825602cf229b5ff24861dd3
Vitaliy Fedoriv <vitaliy.fedoriv@gmail.com>
2016-12-27 22:03:21 +0200

    add JsonIgnore annotation to "new" property

--- src/main/java/org/springframework/samples/petclinic/model/BaseEntity.java
+++ src/main/java/org/springframework/samples/petclinic/model/BaseEntity.java
@@ -20,6 +20,8 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.MappedSuperclass;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 /**
  * Simple JavaBean domain object with an id property. Used as a base class for objects needing this property.
  *
@@ -39,7 +41,7 @@ public class BaseEntity {
     public void setId(Integer id) {
         this.id = id;
     }
-
+    @JsonIgnore
     public boolean isNew() {
         return this.id == null;
     }
