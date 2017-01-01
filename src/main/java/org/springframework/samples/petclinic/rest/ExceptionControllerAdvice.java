47391377d9feea8e410e467d0a2063cd91f821aa
Vitaliy Fedoriv <vitaliy.fedoriv@gmail.com>
2017-01-01 16:49:30 +0200

    improve Exception controller response

--- src/main/java/org/springframework/samples/petclinic/rest/ExceptionControllerAdvice.java
+++ src/main/java/org/springframework/samples/petclinic/rest/ExceptionControllerAdvice.java
@@ -20,6 +20,9 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 /**
  * @author Vitaliy Fedoriv
  *
@@ -29,7 +32,25 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 public class ExceptionControllerAdvice {
 
 	@ExceptionHandler(Exception.class)
-	public ResponseEntity<Exception> exception(Exception e) {
-		return ResponseEntity.badRequest().body(e);
+	public ResponseEntity<String> exception(Exception e) {
+		ObjectMapper mapper = new ObjectMapper();
+		ErrorInfo errorInfo = new ErrorInfo(e);
+		String respJSONstring = "{}";
+		try {
+			respJSONstring = mapper.writeValueAsString(errorInfo);
+		} catch (JsonProcessingException e1) {
+			e1.printStackTrace();
+		}
+		return ResponseEntity.badRequest().body(respJSONstring);
+	}
+	
+	private class ErrorInfo {
+	    public final String className;
+	    public final String exMessage;
+
+	    public ErrorInfo(Exception ex) {
+	        this.className = ex.getClass().getName();
+	        this.exMessage = ex.getLocalizedMessage();
+	    }
 	}
 }
