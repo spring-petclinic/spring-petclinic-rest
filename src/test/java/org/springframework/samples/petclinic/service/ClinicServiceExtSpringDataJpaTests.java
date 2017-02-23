d058a95299ad2c10b3baa9d46e1540268a5d6936
Antoine Rey <antoine.rey@gmail.com>
2017-02-23 18:02:30 +0100

    Migrate to Spring Boot

--- src/test/java/org/springframework/samples/petclinic/service/ClinicServiceExtSpringDataJpaTests.java
+++ src/test/java/org/springframework/samples/petclinic/service/ClinicServiceExtSpringDataJpaTests.java
@@ -16,20 +16,25 @@
 package org.springframework.samples.petclinic.service;
 
 import org.junit.runner.RunWith;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
 
 /**
  * <p> Integration test using the jpa profile.
  *
- * @author Vitaliy Fedoriv 
+ * @author Vitaliy Fedoriv
  * @see AbstractClinicServiceExtTests AbstractClinicServiceExtTests for more details. </p>
  */
-@ContextConfiguration(locations = {"classpath:spring/business-config.xml"})
-@RunWith(SpringJUnit4ClassRunner.class)
-@ActiveProfiles("spring-data-jpa")
-
+@RunWith(SpringRunner.class)
+@DataJpaTest(includeFilters = @ComponentScan.Filter(value = ClinicService.class, type = FilterType.ASSIGNABLE_TYPE))
+@Transactional(propagation = Propagation.NOT_SUPPORTED)
 public class ClinicServiceExtSpringDataJpaTests extends AbstractClinicServiceExtTests {
 
 }
