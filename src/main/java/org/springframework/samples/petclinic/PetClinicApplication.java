170046b4dffd92b1202c37aeee87dfbfc2a2777c
Vitaliy Fedoriv <vitaliy.fedoriv@gmail.com>
2018-05-16 19:34:17 +0300

    change Initializer and switch to Java 8

--- src/main/java/org/springframework/samples/petclinic/PetClinicApplication.java
+++ src/main/java/org/springframework/samples/petclinic/PetClinicApplication.java
@@ -2,9 +2,16 @@ package org.springframework.samples.petclinic;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class PetClinicApplication {
+public class PetClinicApplication extends SpringBootServletInitializer {
+	
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(PetClinicApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(PetClinicApplication.class, args);
