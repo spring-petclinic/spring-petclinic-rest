547116a12ef465a70acf682d9c47209974d6dddf
Vitaliy Fedoriv <vitaliy.fedoriv@gmail.com>
2018-06-21 20:32:28 +0300

    update database scripts

--- src/main/resources/db/hsqldb/insert_authentication.sql
+++ src/main/resources/db/hsqldb/insert_authentication.sql
@@ -1,9 +1,9 @@
 INSERT INTO OAUTH_CLIENT_DETAILS(CLIENT_ID, RESOURCE_IDS, CLIENT_SECRET, SCOPE, AUTHORIZED_GRANT_TYPES, AUTHORITIES, ACCESS_TOKEN_VALIDITY, REFRESH_TOKEN_VALIDITY)
-	VALUES ('spring-security-oauth2-read-client', 'resource-server-rest-api',
-	/*spring-security-oauth2-read-client-password1234*/'$2a$04$WGq2P9egiOYoOFemBRfsiO9qTcyJtNRnPKNBl5tokP7IP.eZn93km',
+	VALUES ('read-client', 'rest-server',
+	/*read-client-password123*/'$2a$04$9FpRAy2E1NqHK35GwZvHee2RJlyshqyEnHq0e5r0mzB7GLjQQ0NKG',
 	'read', 'password,authorization_code,refresh_token,implicit', 'USER', 10800, 2592000);
 
 INSERT INTO OAUTH_CLIENT_DETAILS(CLIENT_ID, RESOURCE_IDS, CLIENT_SECRET, SCOPE, AUTHORIZED_GRANT_TYPES, AUTHORITIES, ACCESS_TOKEN_VALIDITY, REFRESH_TOKEN_VALIDITY)
-	VALUES ('spring-security-oauth2-read-write-client', 'resource-server-rest-api',
-	/*spring-security-oauth2-read-write-client-password1234*/'$2a$04$soeOR.QFmClXeFIrhJVLWOQxfHjsJLSpWrU1iGxcMGdu.a5hvfY4W',
-	'read,write', 'password,authorization_code,refresh_token,implicit', 'USER', 10800, 2592000);
+	VALUES ('write-client', 'rest-server',
+	/*write-client-password123*/'$2a$04$aWnuFg.7NMS5mOgYAVZ41.08HWK41mxDMx6fHgOtXU/Ixtl9LOTei',
+	'read,write', 'password,authorization_code,refresh_token,implicit', 'USER', 10800, 2592000);	
