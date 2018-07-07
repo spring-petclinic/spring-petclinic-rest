6479aaa4b3b47b646e3f4c7d6f4fa293ebd2791d
Vitaliy Fedoriv <vitaliy.fedoriv@gmail.com>
2018-07-07 11:14:59 +0300

    fix #17 - remove Spring Data Rest from project

---
+++ src/main/java/org/springframework/samples/petclinic/rest/RootRestController.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright 2016-2018 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.springframework.samples.petclinic.rest;
+
+import java.io.IOException;
+
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author Vitaliy Fedoriv
+ *
+ */
+
+@RestController
+@CrossOrigin(exposedHeaders = "errors, content-type")
+@RequestMapping("/")
+public class RootRestController {
+
+	@RequestMapping(value = "/")
+	public void redirectToSwagger(HttpServletResponse response) throws IOException {
+		response.sendRedirect("/petclinic/swagger-ui.html");
+	}
+
+}
+
