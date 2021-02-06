d5ee29a8e7c6ccc3f306889cf9a0f176b807705a
Martin Ponbauer <ponbamar@students.zhaw.ch>
2021-02-06 15:52:20 +0100

    Bump to Spring Boot 2.4.2

--- src/test/java/org/springframework/samples/petclinic/service/clinicService/ClinicServiceJdbcTests.java
+++ src/test/java/org/springframework/samples/petclinic/service/clinicService/ClinicServiceJdbcTests.java
@@ -29,7 +29,7 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
  */
 @SpringBootTest
 @RunWith(SpringJUnit4ClassRunner.class)
-@ActiveProfiles("jdbc, hsqldb")
+@ActiveProfiles({"jdbc", "hsqldb"})
 public class ClinicServiceJdbcTests extends AbstractClinicServiceTests {
 
 
--- src/test/java/org/springframework/samples/petclinic/service/userService/UserServiceJpaTests.java
+++ src/test/java/org/springframework/samples/petclinic/service/userService/UserServiceJpaTests.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 @SpringBootTest
 @RunWith(SpringJUnit4ClassRunner.class)
-@ActiveProfiles("jpa, hsqldb")
+@ActiveProfiles({"jpa", "hsqldb"})
 public class UserServiceJpaTests extends AbstractUserServiceTests {
 
 }
