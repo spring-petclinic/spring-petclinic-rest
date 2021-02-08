8c981e9165e8750ed26efe6285fe309a37570030
Nino Peters <dev@ninopeters.de>
2021-02-08 10:21:43 +0100

    add 'find visits by vet id' functionality to clinic service

--- src/main/java/org/springframework/samples/petclinic/service/ClinicServiceImpl.java
+++ src/main/java/org/springframework/samples/petclinic/service/ClinicServiceImpl.java
@@ -67,7 +67,7 @@ public class ClinicServiceImpl implements ClinicService {
         this.vetRepository = vetRepository;
         this.ownerRepository = ownerRepository;
         this.visitRepository = visitRepository;
-        this.specialtyRepository = specialtyRepository; 
+        this.specialtyRepository = specialtyRepository;
 		this.petTypeRepository = petTypeRepository;
     }
 
@@ -249,14 +249,14 @@ public class ClinicServiceImpl implements ClinicService {
 	@Transactional
 	public void savePet(Pet pet) throws DataAccessException {
 		petRepository.save(pet);
-		
+
 	}
 
 	@Override
 	@Transactional
 	public void saveVisit(Visit visit) throws DataAccessException {
 		visitRepository.save(visit);
-		
+
 	}
 
 	@Override
@@ -270,7 +270,7 @@ public class ClinicServiceImpl implements ClinicService {
 	@Transactional
 	public void saveOwner(Owner owner) throws DataAccessException {
 		ownerRepository.save(owner);
-		
+
 	}
 
 	@Override
@@ -284,8 +284,10 @@ public class ClinicServiceImpl implements ClinicService {
 	public Collection<Visit> findVisitsByPetId(int petId) {
 		return visitRepository.findByPetId(petId);
 	}
-	
-	
 
-
+    @Override
+    @Transactional(readOnly = true)
+    public Collection<Visit> findVisitsByVetId(int vetId) {
+        return visitRepository.findByVetId(vetId);
+    }
 }
