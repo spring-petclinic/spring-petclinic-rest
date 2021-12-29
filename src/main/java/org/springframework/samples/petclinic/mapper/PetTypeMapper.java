83fede1b0f48b2ee36e2b824c8e35602d30a2204
Antoine Rey <antoine.rey@free.fr>
2021-12-29 10:50:09 +0100

    Fix PetType creation from the POST /pettypes API

--- src/main/java/org/springframework/samples/petclinic/mapper/PetTypeMapper.java
+++ src/main/java/org/springframework/samples/petclinic/mapper/PetTypeMapper.java
@@ -3,6 +3,7 @@ package org.springframework.samples.petclinic.mapper;
 import org.mapstruct.Mapper;
 import org.springframework.samples.petclinic.rest.dto.PetTypeDto;
 import org.springframework.samples.petclinic.model.PetType;
+import org.springframework.samples.petclinic.rest.dto.PetTypeFieldsDto;
 
 import java.util.Collection;
 import java.util.List;
@@ -14,6 +15,8 @@ import java.util.List;
 public interface PetTypeMapper {
 
     PetType toPetType(PetTypeDto petTypeDto);
+
+    PetType toPetType(PetTypeFieldsDto petTypeFieldsDto);
 
     PetTypeDto toPetTypeDto(PetType petType);
 
