b29ae4b88424301fe6b2236c6ee2cd71cc30cbf4
suryaprakashrajav <surya.p14@gmail.com>
2022-09-07 18:27:54 -0400

    remove reformatted code

--- src/main/java/org/springframework/samples/petclinic/config/SwaggerConfig.java
+++ src/main/java/org/springframework/samples/petclinic/config/SwaggerConfig.java
@@ -34,35 +34,32 @@ import io.swagger.v3.oas.models.info.License;
  */
 @Configuration
 public class SwaggerConfig {
-	
+
     @Bean
     OpenAPI customOpenAPI() {
 
-        return new OpenAPI()
-                .components(new Components())
-                .info(new Info()
-                		.title("REST Petclinic backend Api Documentation")
-                		.version("1.0")
-                		.termsOfService("Petclinic backend terms of service")
-                		.description("This is REST API documentation of the Spring Petclinic backend. If authentication is enabled, when calling the APIs use admin/admin")
-                		.license(swaggerLicense())
-                		.contact(swaggerContact()));
+        return new OpenAPI().components(new Components()).info(new Info()
+                .title("REST Petclinic backend Api Documentation").version("1.0")
+                .termsOfService("Petclinic backend terms of service")
+                .description(
+                        "This is REST API documentation of the Spring Petclinic backend. If authentication is enabled, when calling the APIs use admin/admin")
+                .license(swaggerLicense()).contact(swaggerContact()));
     }
-    
+
     private Contact swaggerContact() {
-    	Contact petclinicContact = new Contact();
-    	petclinicContact.setName("Vitaliy Fedoriv");
-    	petclinicContact.setEmail("vitaliy.fedoriv@gmail.com");
-    	petclinicContact.setUrl("https://github.com/spring-petclinic/spring-petclinic-rest");
-    	return petclinicContact;
+        Contact petclinicContact = new Contact();
+        petclinicContact.setName("Vitaliy Fedoriv");
+        petclinicContact.setEmail("vitaliy.fedoriv@gmail.com");
+        petclinicContact.setUrl("https://github.com/spring-petclinic/spring-petclinic-rest");
+        return petclinicContact;
     }
-    
+
     private License swaggerLicense() {
-    	License petClinicLicense = new License();
-    	petClinicLicense.setName("Apache 2.0");
-    	petClinicLicense.setUrl("http://www.apache.org/licenses/LICENSE-2.0");
-    	petClinicLicense.setExtensions(Collections.emptyMap());
-    	return petClinicLicense;
+        License petClinicLicense = new License();
+        petClinicLicense.setName("Apache 2.0");
+        petClinicLicense.setUrl("http://www.apache.org/licenses/LICENSE-2.0");
+        petClinicLicense.setExtensions(Collections.emptyMap());
+        return petClinicLicense;
     }
 
 }
