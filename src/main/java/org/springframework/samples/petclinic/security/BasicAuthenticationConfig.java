098b93f795abd717aa097c7379a8da5a1894f34b
sonyhonto <157215626+sonyhonto@users.noreply.github.com>
2024-03-29 10:00:46 +0100

    /login with enbabled security
    user is hard coded

--- src/main/java/org/springframework/samples/petclinic/security/BasicAuthenticationConfig.java
+++ src/main/java/org/springframework/samples/petclinic/security/BasicAuthenticationConfig.java
@@ -22,29 +22,29 @@ public class BasicAuthenticationConfig  {
     @Autowired
     private DataSource dataSource;
 
-    @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        // @formatter:off
-        http
-            .authorizeHttpRequests((authz) -> authz
-                .anyRequest().authenticated()
-                )
-                .httpBasic()
-                    .and()
-                .csrf()
-                    .disable();
-        // @formatter:on
-        return http.build();
-    }
+    // @Bean
+    // public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+    //     // @formatter:off
+    //     http
+    //         .authorizeHttpRequests((authz) -> authz
+    //             .anyRequest().authenticated()
+    //             )
+    //             .httpBasic()
+    //                 .and()
+    //             .csrf()
+    //                 .disable();
+    //     // @formatter:on
+    //     return http.build();
+    // }
 
-    @Autowired
-    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-        // @formatter:off
-        auth
-            .jdbcAuthentication()
-                .dataSource(dataSource)
-                .usersByUsernameQuery("select username,password,enabled from users where username=?")
-                .authoritiesByUsernameQuery("select username,role from roles where username=?");
-        // @formatter:on
-    }
+    // @Autowired
+    // public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+    //     // @formatter:off
+    //     auth
+    //         .jdbcAuthentication()
+    //             .dataSource(dataSource)
+    //             .usersByUsernameQuery("select username,password,enabled from users where username=?")
+    //             .authoritiesByUsernameQuery("select username,role from roles where username=?");
+    //     // @formatter:on
+    // }
 }
--- src/main/java/org/springframework/samples/petclinic/security/DisableSecurityConfig.java
+++ src/main/java/org/springframework/samples/petclinic/security/DisableSecurityConfig.java
@@ -1,29 +1,74 @@
 package org.springframework.samples.petclinic.security;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.ProviderManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 /**
- * Starting from Spring Boot 2, if Spring Security is present, endpoints are secured by default
+ * Starting from Spring Boot 2, if Spring Security is present, endpoints are
+ * secured by default
  * using Spring Security’s content-negotiation strategy.
  */
 @Configuration
-@ConditionalOnProperty(name = "petclinic.security.enable", havingValue = "false")
+@EnableWebSecurity
+// @EnableMethodSecurity(securedEnabled = true, jsr250Enabled = true)
+@ConditionalOnProperty(name = "petclinic.security.enable", havingValue = "true")
 public class DisableSecurityConfig {
+
+    @Value("${spring.security.debug:false}")
+    boolean securityDebug;
+
+    @Bean
+    public AuthenticationManager authenticationManager(
+            UserDetailsService userDetailsService,
+            PasswordEncoder passwordEncoder) {
+        DaoAuthenticationProvider authenticationProvider = new DaoAuthenticationProvider();
+        authenticationProvider.setUserDetailsService(userDetailsService);
+        authenticationProvider.setPasswordEncoder(passwordEncoder);
+
+        ProviderManager providerManager = new ProviderManager(authenticationProvider);
+        providerManager.setEraseCredentialsAfterAuthentication(false);
+
+        return providerManager;
+    }
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        // @formatter:off
-        http
-            .authorizeHttpRequests((authz) -> authz
-               .anyRequest().permitAll()
-            )
-            .csrf()
-                .disable();
-        // @formatter:on
+        http.csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(
+                        authorizationManagerRequestMatcherRegistry -> authorizationManagerRequestMatcherRegistry
+                                .requestMatchers(HttpMethod.DELETE).hasRole("ADMIN")
+                                .requestMatchers("/admin/**").hasAnyRole("ADMIN")
+                                .requestMatchers("/user/**").hasAnyRole("USER", "ADMIN")
+                                .requestMatchers("/login/**").permitAll()
+                                .anyRequest().authenticated())
+                .httpBasic(Customizer.withDefaults())
+                .sessionManagement(httpSecuritySessionManagementConfigurer -> httpSecuritySessionManagementConfigurer
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+
         return http.build();
     }
+
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        return web -> web.debug(securityDebug)
+                .ignoring()
+                .requestMatchers("/css/**", "/js/**", "/img/**", "/lib/**", "/favicon.ico");
+    }
+
 }
