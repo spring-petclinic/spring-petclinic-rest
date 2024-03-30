23a69ed3565a035248d86eae8ce05616a58012fd
sonyhonto <157215626+sonyhonto@users.noreply.github.com>
2024-03-30 07:28:55 +0100

    password encryption to database
    bcrypt encoding

--- src/main/java/org/springframework/samples/petclinic/security/Encoders.java
+++ src/main/java/org/springframework/samples/petclinic/security/Encoders.java
@@ -2,20 +2,22 @@ package org.springframework.samples.petclinic.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 public class Encoders {
 
-    // @Bean
-    // public PasswordEncoder userPasswordEncoder() {
-    // return new BCryptPasswordEncoder(8);
-    // }
-
-    @SuppressWarnings("deprecation")
     @Bean
-    public static NoOpPasswordEncoder passwordEncoderNoN() {
-        return (NoOpPasswordEncoder) NoOpPasswordEncoder.getInstance();
+    public PasswordEncoder userPasswordEncoder() {
+    return new BCryptPasswordEncoder(8);
     }
+
+    // @SuppressWarnings("deprecation")
+    // @Bean
+    // public static NoOpPasswordEncoder passwordEncoderNoN() {
+    //     return (NoOpPasswordEncoder) NoOpPasswordEncoder.getInstance();
+    // }
 
 }
--- src/main/java/org/springframework/samples/petclinic/security/RegisterService.java
+++ src/main/java/org/springframework/samples/petclinic/security/RegisterService.java
@@ -3,6 +3,7 @@ package org.springframework.samples.petclinic.security;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.stereotype.Service;
 
@@ -12,11 +13,15 @@ public class RegisterService {
     @Autowired
     UserDetailsManager usersManager;
 
+    @Autowired
+    PasswordEncoder passwordEncoder;
+
     public UserDetails registerNewUserAccount(UserDtoSecurity userDto) {
 
         UserDetails user = User.builder()
                 .username(userDto.getUsername())
                 .password(userDto.getPassword())
+                .password(passwordEncoder.encode(userDto.getPassword()))
                 .roles(userDto.getRoles())
                 .build();
         usersManager.createUser(user);
